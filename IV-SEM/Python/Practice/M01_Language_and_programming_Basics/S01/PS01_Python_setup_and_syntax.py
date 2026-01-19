e94ce9f8aa12de375c42963669a932d8d82bf771
Sri Nitya <chilakapatinitya004@gmail.com>
2026-01-19 09:53:45 +0530

    files updated

---
+++ IV-SEM/Python/Practice/M01_Language_and_programming_Basics/S01/PS01_Python_setup_and_syntax.py
@@ -0,0 +1,4 @@
+user_name1 = "preeti"
+print(user_name1)
+and12 = 100 
+print(and12)
---
+++ IV-SEM/Python/Practice/M01_Language_and_programming_Basics/S01/PS02_Variables_and_data_types.py
@@ -0,0 +1,30 @@
+'''
+DATA TYPES:
+
+1. Fundemental DT:
+   1. int 
+   2. float 
+   3. complex 
+   4. boolean 
+   5. string 
+
+2. Collection DT:
+   1. list 
+   2. tuple 
+   3. set 
+   4. dictionry 
+'''
+x = 10 
+y = 12e3 
+print(x,type(x))
+print(y,type(y))
+
+c1 = 4 + 5j 
+c2 = complex(2,-2)
+print(c1.real)
+print(c1.imag)
+print(c1 + c2)
+print(c1 - c2)
+print(c1 * c2)
+print(c1 / c2)
+
