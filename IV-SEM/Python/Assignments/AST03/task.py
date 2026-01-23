3935ad322b45ede39fb55f5eb20c5847df87a172
Sri Nitya <chilakapatinitya004@gmail.com>
2026-01-23 10:15:42 +0530

    assingment

---
+++ IV-SEM/Python/Assignments/AST03/task.py
@@ -0,0 +1,15 @@
+def Student_Grade_System(name: str, n1: int, n2: int, n3: int) -> str:
+    avg = (n1 + n2 + n3) / 3
+
+    if avg >= 40:
+        status = "Pass"
+    else:
+        status = "Fail"
+
+    return f"Average grade: {avg:.2f}, Status: {status}"
+
+
+if __name__ == '__main__':
+    name = input()
+    n1, n2, n3 = list(map(int, input().split()))
+    print(Student_Grade_System(name, n1, n2, n3))
